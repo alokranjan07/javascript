f6e1f4f8690af1b8822f8cdca8eb5117359afc84
alokranjan07 <145465021+alokranjan07@users.noreply.github.com>
2024-11-15 05:15:25 +0000

    container config done

---
+++ 02js.js
@@ -0,0 +1,3 @@
+//
+"use strict"; //newer version of javascript 
+alert("hel0");// works in browser not in node js 
---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log("hello");
+//variables
+var var1="ram";
+//there is no scope for the  variable and we havent to use in future
+//use let instead of var  let has scope {}
+let abc="first";
+console.log(abc);
+abc=45;
+//after running this the value gets chganged 
+console.log(abc);
